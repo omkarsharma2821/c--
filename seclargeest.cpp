be819d7b7850dd36817a258cc55045e82d6c3d50
Omkar Sharma <omkarsharma2821@gmail.com>
2023-09-12 17:11:27 +0530

    second largest

---
+++ seclargeest.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int seclargest(int arr[], int n){
+    int i;
+    int largest = INT8_MIN;
+    int seclargest = INT8_MIN;
+    for(i = 0; i<n; i++){
+        if(arr[i]>largest){
+            seclargest = largest;
+            largest = arr[i];
+        }
+    }
+    return seclargest;
+
+}
+
+
+int main(){
+
+    int arr[1000];
+    int n;
+    cout<<"enter the array size"<<endl;
+    cin>>n;
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+int ans= seclargest(arr,n);
+cout<<ans;
+}
