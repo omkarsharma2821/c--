18e28d238d88f449f856501d53f34ac54aaa51b7
Omkar Sharma <omkarsharma2821@gmail.com>
2023-09-19 19:40:07 +0530

    check

--- min_max.cpp
+++ min_max.cpp
@@ -34,9 +34,10 @@ int getmax(int a[],int n){
 int main(){
     int arr[100];
     int size;
-    cout<<"Enter the array size"<<endl;
+    cout<<"Enter the array size : "<<endl;
     cin>>size;
 
+    cout<<"Enter the array element : ";
     for(int i = 0; i<size; i++){
         cin>>arr[i];
     }
